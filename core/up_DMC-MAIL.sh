adaa2d006ce727f549c6edf385e14a6454af21e3
DEVIL-MAY-CRY-444 <101607573+DEVIL-MAY-CRY-444@users.noreply.github.com>
2023-05-29 00:21:04 +0100

    Add files via upload

---
+++ core/up_DMC-MAIL.sh
@@ -0,0 +1,69 @@
+##########################################################
+# DEV : DEVIL😈MAY😈CRY
+# Github : https://github.com/DEVIL-MAY-CRY-444
+# IF YOU WANNA TAKE CREADITS FOR THIS TOOL,,,LOOK YOUR SELF AGAIN AND AGAIN,,
+
+# THANKS TO ALLAH
+# WELCOME HOME
+# 
+#####################৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳################
+
+
+
+a='\033[1;30m'
+rc='\033[1;31m'
+g='\033[1;32m' 
+y='\033[1;33m'
+b='\033[1;34m'
+p='\033[1;35m'
+c='\033[1;36m' 
+w='\033[1;37m'
+m='\033[1;94m'
+n='\033[0;00m' 
+
+update(){
+  echo -e "${p} A UPDATE AVAILABLE"
+  sleep 0.1
+  echo -e "${m} DMC-MAIL IS UPDATING"
+  sleep 1
+  xdg-open https://www.facebook.com/imalsha.maduranga.3/
+  echo -e "${b} PLEASE WAIT..." 
+  cd ; 
+  rm -rf DMC-MAIL ; 
+  apt update ; 
+  apt install python3 -y ; 
+  apt install git -y ; 
+  pip install requests ; 
+  git clone https://github.com/DEVIL-MAY-CRY-444/DMC-MAIL
+  
+
+
+  echo
+  echo -e "${g} NOW YOUR TOOL UPDATED."
+  echo
+  echo -e "${y} THANKS FOR UPDATE ME."
+  echo
+  echo -e "${g}<==> NOW TYPE <==>"
+  echo
+  echo -e "${b} cd ; cd DMC-MAIL ; python DMC.py${n}" ;
+  sleep 3
+
+
+}
+update
+echo 
+echo
+exit
+
+
+
+
+
+##########################################################
+# DEV : DEVIL😈MAY😈CRY
+# Github : https://github.com/DEVIL-MAY-CRY-444
+# IF YOU WANNA TAKE CREADITS FOR THIS TOOL,,,LOOK YOUR SELF AGAIN AND AGAIN,,
+# THANKS TO ALLAH
+# WELCOME HOME
+# MAO2116
+#####################৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳################
